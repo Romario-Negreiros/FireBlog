9028ab8e032475f6e56d8212086850d65976f0bc
Romario Negreiros <nromario482@gmail.com>
2021-08-21 14:33:35 -0300

    firebase configurations + global variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,18 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DB,
     projectId: process.env.REACT_APP_PID,
     storageBucket: process.env.REACT_APP_SB,
     messagingSenderId: process.env.REACT_APP_SID,
     appId: process.env.REACT_APP_APPID,
     measurementId: process.env.REACT_APP_MID
 };
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+export const firebaseAuth = firebase.auth();
+export const firebaseDatabase = firebase.database().ref();
+export const firebaseStorage = firebase.storage().ref();
