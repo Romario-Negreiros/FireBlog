e28fca1d2692c18f4c17715f05903f133a9c2e03
Romario Negreiros <nromario482@gmail.com>
2021-08-23 15:00:34 -0300

    starting to create navbar and home components

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import { Container } from './styles';
+
+const Home: FC = () => {
+    return (
+        <Container>
+            Hello world
+        </Container>
+    )
+}
+
+export default Home;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import { Container } from './styles';
+
+const NavBar: FC = () => {
+    return (
+        <Container>
+            Hello world
+        </Container>
+    )
+}
+
+export default NavBar;
