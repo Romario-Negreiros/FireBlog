de05fdc7936eefef4b2a7380b52bd2f0b0d2fb56
Romario Negreiros <nromario482@gmail.com>
2021-08-24 11:19:03 -0300

    created themes file

---
+++ src/themes.ts
@@ -0,0 +1,17 @@
+const themes = {
+    backgrounds: {
+        primaryblue: '#081229',
+        secondaryblue: '#191d3a',
+    },
+    colors: {
+        pink: '#ec5990',
+        error: '#ff0008',
+    },
+    clamps: {
+        headings: 'clamp(2rem, 1rem + 1.5vw, 5rem)',
+        labels: 'clamp(1.6rem, 1.5vw, 3rem)',
+        texts: 'clamp(1.4rem, 1.3vw, 2.5rem)',
+    },
+};
+
+export default themes;
