46243acd9529254c814a3da48ffff03db54721e5
Romario Negreiros <nromario482@gmail.com>
2021-08-24 00:21:21 -0300

    created menu icon + media query for mobile menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,36 +1,33 @@
 import { FC } from 'react';
-import { Container, Navigation, Link } from './styles';
+import { Container, Navigation, Link, Burguer } from './styles';
 
 const NavBar: FC = () => {
     return (
         <Container>
             <h2>FireBlog</h2>
             <Navigation>
+                <Burguer>
+                    <span className="close-right"></span>
+                    <span className="close-left"></span>
+                    <span className="fade"></span>
+                </Burguer>
                 <ul>
                     <li>
-                        <Link to="/">
-                            Home
-                        </Link>
+                        <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/">
-                            Most visited
-                        </Link>
+                        <Link to="/">Most visited</Link>
                     </li>
                     <li>
-                        <Link to="/">
-                            Portfólio
-                        </Link>
+                        <Link to="/">Portfólio</Link>
                     </li>
                     <li>
-                        <Link to="/">
-                            Github
-                        </Link>
+                        <Link to="/">Github</Link>
                     </li>
                 </ul>
             </Navigation>
         </Container>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -23,16 +23,66 @@ export const Navigation = styled.nav`
         display: flex;
         justify-content: space-between;
         align-items: center;
+        @media screen and (max-width: 600px) {
+            display: none;
+            position: absolute;
+            top: 0;
+            left: 0;
+            flex-direction: column;
+            height: 100vh;
+            width: 100vw;
+            background: #191d3a;
+            z-index: 10;
+        }
+        .mobile-nav--active {
+            display: flex;
+        }
     }
     li {
         margin-left: 30px;
         height: 100%;
+        @media screen and (max-width: 600px) {
+            margin-left: 0;
+            margin-top: 3rem;
+        }
+    }
+`;
+
+export const Burguer = styled.div`
+    display: none;
+    @media screen and (max-width: 600px) {
+        cursor: pointer;
+        display: block;
+        position: absolute;
+        top: 22px;
+        right: 25px;
+        z-index: 11;
+    }
+    span {
+        @media screen and (max-width: 600px) {
+            background: #ec5990;
+            width: 15px;
+            height: 2px;
+            display: block;
+            margin-top: 3px;
+            background: $weakGray;
+            transition: all 0.4s ease-in-out;
+        }
+        .close-left {
+            transform: translate(0, 2px) rotate(45deg);
+        }
+        .close-right {
+            transform: translate(0, -3px) rotate(135deg);
+        }
+        .fade {
+            opacity: 0;
+        }
     }
 `;
 
 export const Link = styled(ReactRouterLink)`
     color: #ff99ff;
-    padding: 5px;
+    padding: 0.5rem;
     font-size: 1.6rem;
     height: 100%;
     display: block;
@@ -40,5 +90,12 @@ export const Link = styled(ReactRouterLink)`
     :hover {
         background: rgba(25%, 29%, 58%, 0.5);
         color: #ec5990;
+    }
+    @media screen and (max-width: 600px) {
+        line-height: 0;
+        height: fit-content;
+        padding: 2rem 0;
+        width: 100vw;
+        text-align: center;
     }
 `;
