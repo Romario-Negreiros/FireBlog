b0349940fa34a4e8c82ad6bb4315bacfa5f9a759
Romario Negreiros <nromario482@gmail.com>
2021-08-26 16:39:29 -0300

    created routes for main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,17 +1,25 @@
 // Modules or lib content
 import { FC } from 'react';
+import { Switch, Route } from 'react-router-dom';
 // Components
-import { NavBar, Home } from '../../components/index';
-
+import { NavBar, Home, CreatePosts, ManagePosts } from '../../components/index';
 const Main: FC = () => {
-
-
     return (
         <>
             <NavBar />
-            <Home />
+            <Switch>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
+                <Route path="/home/create">
+                    <CreatePosts />
+                </Route>
+                <Route path="/home/manage">
+                    <ManagePosts />
+                </Route>
+            </Switch>
         </>
-    )
-}
+    );
+};
 
 export default Main;
