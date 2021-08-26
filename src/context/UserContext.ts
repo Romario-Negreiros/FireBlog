07b916c299ecfb6a5c974852cc3ef794ef9c1aea
Romario Negreiros <nromario482@gmail.com>
2021-08-25 23:23:04 -0300

    context for user data

---
+++ src/context/UserContext.ts
@@ -0,0 +1,16 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+
+export interface UserData {
+    userID: string,
+    name: string,
+    email: string,
+}
+
+interface User {
+    userData: UserData | null,
+    setUserData: Dispatch<SetStateAction<UserData | null>>
+}
+
+const userContext = createContext<User | null>(null);
+
+export default userContext;
