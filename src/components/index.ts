eccaaef2782571ec6a9e9ae96e400a31349c2444
Romario Negreiros <nromario482@gmail.com>
2021-08-28 17:04:20 -0300

    exported edit posts component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,9 @@ export { default as Home } from './Home/Home';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Loader } from './Loader/Loader';
 export { default as Portal } from './Portal/Portal';
-export { default as CreatePosts } from './CreatePosts/CreatePosts'
-export { default as ManagePosts } from './ManagePosts/ManagePosts'
+export { default as CreatePosts } from './CreatePosts/CreatePosts';
+export { default as ManagePosts } from './ManagePosts/ManagePosts';
+export { default as EditPosts } from './EditPosts/EditPosts';
 
 // Modal
 export { default as CreateAccount } from './Portal/Modals/CreateAccount';
