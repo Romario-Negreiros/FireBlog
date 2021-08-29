e1f8d4e9459f665166da70fe2e4047b91cfe0420
Romario Negreiros <nromario482@gmail.com>
2021-08-29 13:54:49 -0300

    created route for posts for category component + created state for drop down and posts form communication

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,18 +1,21 @@
 // Modules or lib content
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 // Components
-import { NavBar, Home, CreatePosts, ManagePosts, EditPosts, Post } from '../../components/index';
+import { NavBar, Home, CreatePosts, ManagePosts, EditPosts, Post, PostsForCategory } from '../../components/index';
 const Main: FC = () => {
+
+    const [hasPostsChanged, setHasPostsChanged] = useState<boolean>(false);
+
     return (
         <>
-            <NavBar />
+            <NavBar hasPostsChanged={hasPostsChanged} setHasPostsChanged={setHasPostsChanged} />
             <Switch>
                 <Route exact path="/home">
                     <Home />
                 </Route>
                 <Route path="/home/create/:userID">
-                    <CreatePosts />
+                    <CreatePosts setHasPostsChanged={setHasPostsChanged} />
                 </Route>
                 <Route path="/home/manage/:userID">
                     <ManagePosts />
@@ -22,6 +25,9 @@ const Main: FC = () => {
                 </Route>
                 <Route path="/home/posts/:postID">
                     <Post />
+                </Route>
+                <Route path="/home/categories/:category">
+                    <PostsForCategory />
                 </Route>
             </Switch>
         </>
