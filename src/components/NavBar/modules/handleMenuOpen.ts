c1a48406f3c32cc88cc1ff9eb8eccb3cd9a80607
Romario Negreiros <nromario482@gmail.com>
2021-09-12 13:28:05 -0300

    fixed handle menu open function being executed when there is no mobile menu

--- src/components/NavBar/modules/handleMenuOpen.ts
+++ src/components/NavBar/modules/handleMenuOpen.ts
@@ -1,9 +1,14 @@
-const handleMenuOpen = (setIsOpen: (isOpen: boolean) => void, isOpen: boolean) => {
+const handleMenuOpen = (
+    setIsOpen: (isOpen: boolean) => void,
+    isOpen: boolean
+) => {
     setIsOpen(!isOpen);
-    document.querySelector('.first')?.classList.toggle('close-left')
-    document.querySelector('.second')?.classList.toggle('close-right')
-    document.querySelector('.third')?.classList.toggle('fade')
-    document.querySelector('body')?.classList.toggle('body-on-menu-active')
+    if (window.innerWidth <= 700) {
+        document.querySelector('.first')?.classList.toggle('close-left');
+        document.querySelector('.second')?.classList.toggle('close-right');
+        document.querySelector('.third')?.classList.toggle('fade');
+        document.querySelector('body')?.classList.toggle('body-on-menu-active');
+    }
 };
 
 export default handleMenuOpen;
