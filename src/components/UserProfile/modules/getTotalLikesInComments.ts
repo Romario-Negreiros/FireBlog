479f2b539bca19d1c2bf58a306341e3328184d73
Romario Negreiros <nromario482@gmail.com>
2022-08-13 14:47:27 -0300

    refactor: fix forEach is not a function error

--- src/components/UserProfile/modules/getTotalLikesInComments.ts
+++ src/components/UserProfile/modules/getTotalLikesInComments.ts
@@ -7,22 +7,20 @@ const getTotalLikesInComments = async (username: string): Promise<number> => {
     let totalLikesInComments: number = 0;
     const response = await firebaseDatabase.child('posts').get();
     const getValues = Object.values(response.val());
-    const getPosts: PostObject[][] = getValues.map(postObj =>
-        Object.values(postObj as { postId: PostObject })
-    );
-    const getAllComments: CommentsType[][] = getPosts.map(postArr => {
-        return postArr.map(post => JSON.parse(post.comments));
+    const getPosts: PostObject[] = getValues.map(postObj => {
+        const post = Object.values(postObj as { postId: PostObject });
+        return post[0];
     });
-    getAllComments.forEach(commentsForPost => {
-        commentsForPost.forEach(commentsArr => {
-            commentsArr.forEach(comment => {
-                if (comment.author === username) {
-                    comment.rating.forEach(rate => {
-                        if (rate.like) totalLikesInComments += 1;
-                    });
-                }
+    const getAllComments: CommentsType = getPosts.map(post => {
+        const comments = JSON.parse(JSON.parse(post.comments));
+        return comments[0];
+    });
+    getAllComments.forEach(comment => {
+        if (comment.author === username) {
+            comment.rating.forEach(rate => {
+                if (rate.like) totalLikesInComments += 1;
             });
-        });
+        }
     });
     return totalLikesInComments;
 };
